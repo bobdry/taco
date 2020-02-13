07e6a0bd2c997320d5d407675841cc573c9f89e1
Bob Dry <bobdry@Bobs-MacBook-Air.local>
2020-02-13 11:20:41 -0500

    adding icons, UI round one is done

--- src/TacoHook.js
+++ src/TacoHook.js
@@ -113,7 +113,7 @@ function TacoHook() {
                 <ResetOrder handleResetOrder={handleResetOrder}/>
             </div> 
 
-            <div class="monkik-icons">Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <div className="monkik-icons">Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   
         </div> 
     ); 
