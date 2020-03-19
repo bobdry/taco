565eea69bfaf3d5f455d237f9708eb560d7c5907
Bob Dry <bobdry@Bobs-MacBook-Air.local>
2020-03-19 15:45:25 -0400

    Updates

--- precache-manifest.ffcfb37d3eacdc8ceb319a99cdc9b5df.js
+++ precache-manifest.ffcfb37d3eacdc8ceb319a99cdc9b5df.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "33c3b35e85e1e93f795666b8e80aa741",
+    "revision": "dce4c0b0716c0879ef22eafe0be4019a",
     "url": "/taco/index.html"
   },
   {
-    "revision": "e388d754b7bd933e8f26",
-    "url": "/taco/static/css/main.e01954b1.chunk.css"
+    "revision": "d854036c0b22bec4fb59",
+    "url": "/taco/static/css/main.c0e66921.chunk.css"
   },
   {
     "revision": "c5f55d7ded94c9ba416f",
     "url": "/taco/static/js/2.50fdec36.chunk.js"
   },
   {
-    "revision": "e388d754b7bd933e8f26",
-    "url": "/taco/static/js/main.c2e337f2.chunk.js"
+    "revision": "d854036c0b22bec4fb59",
+    "url": "/taco/static/js/main.95419536.chunk.js"
   },
   {
     "revision": "0f52085e31d19fb205e7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taco/precache-manifest.ef9fd2b012f53427d993f9f9f7f9de4f.js"
+  "/taco/precache-manifest.ffcfb37d3eacdc8ceb319a99cdc9b5df.js"
 );
 
 self.addEventListener('message', (event) => {
