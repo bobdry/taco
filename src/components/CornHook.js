7930a2c84d79e451c8eec0f3f764f58d42c51a8e
bobdry <bobdry@gmail.com>
2024-02-07 10:19:00 -0500

    new styles

--- src/components/CornHook.js
+++ src/components/CornHook.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function CornHook(props) { 
     return ( 
-        <div className="cornStyle" onClick={props.handleClickCorn} > 
+        <div className="tortilla corn" onClick={props.handleClickCorn} > 
         <p>Corn</p>
             {props.tacoIngredientStack} 
         </div> 
--- src/components/FlourHook.js
+++ src/components/FlourHook.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function FlourHook(props) { 
     return ( 
-        <div className="flourStyle" onClick={props.handleClickFlour} > 
+        <div className="tortilla flour" onClick={props.handleClickFlour} > 
             <p>Flour</p>
             {props.tacoIngredientStack} 
         </div> 
