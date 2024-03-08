26a9521941f449a6fbbaa4bb0683cc3e2f3e768c
bobdry <bobdry@gmail.com>
2024-03-07 19:38:35 -0500

    Updates

---
+++ precache-manifest.2678ca43af4c194d8596a2320b0980be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c066916897d9fff50aaec80f70bba95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa01e4f8c64543ef4bd7",
+    "url": "/static/css/main.9683c3dc.chunk.css"
+  },
+  {
+    "revision": "c5f55d7ded94c9ba416f",
+    "url": "/static/js/2.50fdec36.chunk.js"
+  },
+  {
+    "revision": "aa01e4f8c64543ef4bd7",
+    "url": "/static/js/main.9d827684.chunk.js"
+  },
+  {
+    "revision": "37e6b53f405e1193b970",
+    "url": "/static/js/runtime-main.564b2370.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taco/precache-manifest.33c263c36e6d780a84b36e1cf3d3b7b5.js"
+  "/precache-manifest.2678ca43af4c194d8596a2320b0980be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taco/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
